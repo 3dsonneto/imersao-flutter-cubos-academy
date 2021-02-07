cc1071917294316e2260865da9759fce0939e56e
Edson <edson_neto1996@hotmail.com>
2021-02-06 22:15:34 -0300

    Removendo banner de debug

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:trivia_academy/initial_screen.dart';
 import 'initial_screen.dart';
 
-void main(){
+void main() {
   runApp(Trivia());
 }
 
@@ -10,7 +10,6 @@ class Trivia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InitialScreen()
-    );
+        debugShowCheckedModeBanner: false, home: InitialScreen());
   }
 }
